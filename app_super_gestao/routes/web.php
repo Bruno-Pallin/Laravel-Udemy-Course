5ede34d52d9850af85ee798dcd0510f1216b8560
Bruno Pallin de Azevedo <pallinb@hotmail.com>
2022-09-01 22:45:07 -0300

    How to group routes to do private parts of my application

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -13,25 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*
-    Route::get($uri, $callBackFunction);
-
-http verb:
-    get/post/put/patch/delete/options
-*/
 
 Route::get('/', 'MainController@main');
-
 Route::get('/about-us', 'AboutUsController@aboutUs');
-
 Route::get('/contact', 'ContactController@contact');
+Route::get('/login', function(){ return 'Login'; });
 
-Route::get(
-    '/contact/{name}/{category_id?}',
-    function (
-        string $name = 'Default Name', 
-        int $category_id = 1 //Info
-        ) {
-        echo "We are here: $name - $category_id";
-    }
-)->where('category_id', '[0-9]+')->where('name', '[A-Za-z]+');
+Route::prefix('/app')->group(function(){
+    Route::get('/customers', function(){ return 'Customers'; });
+    Route::get('/providers', function(){ return 'Providers'; });
+    Route::get('/products', function(){ return 'Products'; });
+});
+
+/*
+    First Contact With Route
+    Route::get($uri, $callBackFunction);
+
+    http verb:
+        get/post/put/patch/delete/options
+*/
+/*
+Learning About params
+    Route::get(
+        '/contact/{name}/{category_id?}',
+        function (
+            string $name = 'Default Name', 
+            int $category_id = 1 //Info
+            ) {
+            echo "We are here: $name - $category_id";
+        }
+    )->where('category_id', '[0-9]+')->where('name', '[A-Za-z]+');
+*/
