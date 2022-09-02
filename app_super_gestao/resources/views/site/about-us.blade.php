425108fd4f5b2d997ccc5f86439ac384435c1146
Bruno Pallin de Azevedo <pallinb@hotmail.com>
2022-09-01 22:31:58 -0300

    We use our routes as href in navigation links in the site

--- app_super_gestao/resources/views/site/about-us.blade.php
+++ app_super_gestao/resources/views/site/about-us.blade.php
@@ -8,5 +8,10 @@
 </head>
 <body>
     <h3>About-Us View</h3>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about-us">About-Us</a></li>
+        <li><a href="/contact">Contact</a></li>
+    </ul>
 </body>
 </html>
--- app_super_gestao/resources/views/site/main.blade.php
+++ app_super_gestao/resources/views/site/main.blade.php
@@ -8,5 +8,10 @@
 </head>
 <body>
     <h3>Home View</h3>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about-us">About-Us</a></li>
+        <li><a href="/contact">Contact</a></li>
+    </ul>
 </body>
 </html>
